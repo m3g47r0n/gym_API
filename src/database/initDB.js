85269e40817f6a82889ebfe320624b0769c69636
Will <willsr@protonmail.com>
2023-08-02 17:32:06 +0200

    Corrección de URL de GIFs en initDB

--- src/database/initDB.js
+++ src/database/initDB.js
@@ -6,175 +6,6 @@ const { getConnection } = require('./db');
 
 //Creamons la función insertExercises para poder precargar ejercicios en nuestra aplicación
 async function insertExercises(connection) {
-<<<<<<< HEAD
-    try {
-        console.log('Carga de ejercicios en la base de datos');
-
-        //Ejercicios precargados
-        const exercisesList = [
-            {
-                name: 'Francés horizontal',
-                description: 'Túmbate de espaldas. Coge la barra con ambas manos y estira los brazos hacia el techo. Baja la barra tan solo doblando los codos y repite.',
-                picture: 'https://tenor.com/view/workout-weights-dumbbells-arm-exercise-gif-11959862',
-                type: 'Fuerza',
-                muscleGroupId: 1,
-                createdAt: new Date(),
-            },
-            {
-                name: 'Extensiones erguidas',
-                description: 'De pie, dobla ligeramente las rodillas e inclina la espalda hacia delante manteniéndola siempre recta. Con una mancuerna en cada mano, contrae el bíceps y estira el brazo totalmente hacia atrás.',
-                picture: 'https://tenor.com/view/exercise-workout-arm-exercise-dumbbells-gif-11959859',
-                type: 'Fuerza',
-                muscleGroupId: 1,
-                createdAt: new Date(),
-            },
-            {
-                name: 'Zancadas',
-                description: 'Coge una mancuerna en cada mano. Colócate de pie mirando siempre al frente y la espalda recta. Da una zancada hacia delante y dobla la rodilla de la pierna extendida hasta formar un ángulo de 90º.',
-                picture: 'https://tenor.com/view/lunges-exercise-workout-leg-exercise-working-out-gif-17352592',
-                type: 'Equilibrio',
-                muscleGroupId: 2,
-                createdAt: new Date(),
-            },
-            {
-                name: 'Sentadillas de sumo',
-                description: 'Abre las piernas, mirando al frente y espalda siempre recta. Baja la pélvis hasta debajo de la altura de las rodillas y cierra los brazos juntando las manos delante de la cara.',
-                picture: 'https://tenor.com/view/sumo-squat-exercise-workout-working-out-gif-17352727',
-                type: 'Flexibilidad',
-                muscleGroupId: 2,
-                createdAt: new Date(),
-            },
-            {
-                name: 'Extensión rodilla-mancuernas',
-                description: 'Erguido, coge una mancuerna en cada mano y colócalas, con los brazos extendidos, delante de loscuádriceps. Agacha la espalda, siempre recta, doblando ligeramente las rodillas.',
-                picture: 'https://tenor.com/view/workouts-deadlift-gif-24035559',
-                type: 'Flexibilidad',
-                muscleGroupId: 3,
-                createdAt: new Date(),
-            },
-            {
-                name: 'Remo con mancuernas',
-                description: 'Coge una mancuerna en cada mano. De pie, agacha la espalda, siempre recta, y extiende los brazos hacia abajo de modo que apunten al suelo. Trae las mancuernas hacia el pecho usando la espalda y doblando el codo.',
-                picture: 'https://tenor.com/view/dumbbell-row-standing-gif-10530352',
-                type: 'Fuerza',
-                muscleGroupId: 3,
-                createdAt: new Date(),
-            },
-            {
-                name: 'Press banca inclinado mancuerna',
-                description: 'Coge una mancuerna en cada mano. Acuéstate en el banco con el respaldo reclinado. Estira ambos brazos hacia el techo y repite.',
-                picture: 'https://tenor.com/view/2inclne-dumbel-press-gif-26653346',
-                type: 'Fuerza',
-                muscleGroupId: 4,
-                createdAt: new Date(),
-            },
-            {
-                name: 'Press banca barra horizontal',
-                description: 'Acuéstate horizontalmente en un banco. Con las dos manos, coge la barra y elévala hacia el techo, extendiendo ambos brazo. Baja hasta tocar el pecho y repite.',
-                picture: 'https://tenor.com/view/bench-press-gif-26543726',
-                type: 'Fuerza',
-                muscleGroupId: 4,
-                createdAt: new Date(),
-            },
-            {
-                name: 'Press Arnold',
-                description: 'Sentado en un banco, coge una mancuerna en cada mano. Eléva los brazos por encima de la cabeza. Al bajarlos, trae las mancuernas hacia delante de la cara girando las muñecas de tal forma que las palmas estén mirando hacia adentro.',
-                picture: 'https://tenor.com/view/arnold-press-gif-25588656',
-                type: 'Fuerza',
-                muscleGroupId: 5,
-                createdAt: new Date(),
-            },
-            {
-                name: 'Pájaro erguido',
-                description: 'Coge una mancuerna en cada mano. De pie, agacha la espalda hacia delante. Con los brazos estidado y las manos enfrentadas, eleva los brazos lateralmente, como las alas de un pájaro.',
-                picture: 'https://tenor.com/view/rear-raise-rear-rear-raise-gym-gif-27091664',
-                type: 'Fuerza',
-                muscleGroupId: 5,
-                createdAt: new Date(),
-            }
-        ];
-
-        //Introducimos los ejercicios en la tabla "exercises"
-        for (const exercise of exercisesList) {
-            await connection.query(
-                `
-                INSERT INTO exercises (name, description, picture, type, muscleGroupId, createdAt) VALUES (?, ?, ?, ?, ?, ?)
-                `,
-                [
-                    exercise.name,
-                    exercise.description,
-                    exercise.picture,
-                    exercise.type,
-                    exercise.muscleGroupId,
-                    exercise.createdAt,
-                ]
-            );
-        }
-        console.log('Ejercicios introducidos correctamente.');
-    } catch (error) {
-        console.log('Error al introducir los ejercicios:', error);
-    }
-}
-
-async function insertWorkout(connection, exercises, workoutType) {
-    try {
-      console.log(`Creando entrenamiento con ejercicios de tipo "${workoutType}"`);
-  
-      // Filtrar los ejercicios por tipo
-      const workoutExercises = exercises.filter(exercise => exercise.type === workoutType);
-  
-      // Crear el entrenamiento con nombre y descripción
-      const workoutName = `Entrenamiento de ${workoutType}`;
-      const workoutDescription = `Entrenamiento de ${workoutType}`;
-      const createdAt = new Date();
-  
-      await connection.query(
-        `
-        INSERT INTO workouts (name, description, createdAt) VALUES (?, ?, ?)
-        `,
-        [workoutName, workoutDescription, createdAt]
-      );
-  
-      // Obtener el ID del entrenamiento que hemos creado
-      const [workoutId] = await connection.query(
-        `
-        SELECT id FROM workouts WHERE name = ? AND description = ? AND createdAt = ?
-        `,
-        [workoutName, workoutDescription, createdAt]
-      );
-  
-      // Insertar los ejercicios del tipo especificado en el entrenamiento
-      for (const exercise of workoutExercises) {
-        await connection.query(
-          `
-          INSERT INTO workouts_exercises (workoutId, exerciseId) VALUES (?, ?)
-          `,
-          [workoutId[0].id, exercise.id]
-        );
-      }
-  
-      console.log(`Entrenamiento creado exitosamente con ejercicios de tipo "${workoutType}".`);
-    } catch (error) {
-      console.error('Error al crear el entrenamiento:', error);
-    }
-}
-
-async function dbConnection() {
-  let connection;
-  const { MYSQL_DATABASE } = process.env;
-  try {
-    //Conexión con la base de datoss
-    connection = await getConnection();
-
-    console.log('Borrando base de datos si existe...');
-    await connection.query(`DROP DATABASE IF EXISTS ${MYSQL_DATABASE};`);
-
-    console.log('Creando base de datos si no existe...');
-    await connection.query(`CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};`);
-
-    await connection.query(`USE ${MYSQL_DATABASE};`);
-
-=======
   try {
     console.log('Carga de ejercicios en la base de datos');
 
@@ -185,7 +16,7 @@ async function dbConnection() {
         description:
           'Túmbate de espaldas. Coge la barra con ambas manos y estira los brazos hacia el techo. Baja la barra solo doblando los codos y repite.',
         picture:
-          'https://tenor.com/view/workout-weights-dumbbells-arm-exercise-gif-11959862',
+          'https://media.tenor.com/Zr4aVZVbkjgAAAAC/workout-weights.gif',
         type: 'Fuerza',
         muscleGroupId: 1,
         createdAt: new Date(),
@@ -195,7 +26,7 @@ async function dbConnection() {
         description:
           'De pie, dobla ligeramente las rodillas e inclina la espalda hacia delante manteniéndola siempre recta. Con una mancuerna en cada mano, contrae el bíceps y estira el brazo totalmente hacia atrás.',
         picture:
-          'https://tenor.com/view/exercise-workout-arm-exercise-dumbbells-gif-11959859',
+          'https://media.tenor.com/ZuBIctV9gLsAAAAC/exercise-workout.gif',
         type: 'Fuerza',
         muscleGroupId: 1,
         createdAt: new Date(),
@@ -205,7 +36,7 @@ async function dbConnection() {
         description:
           'Coge una mancuerna en cada mano. Colócate de pie mirando siempre al frente y la espalda recta. Da una zancada hacia delante y dobla la rodilla de la pierna extendida hasta formar un ángulo de 90º.',
         picture:
-          'https://tenor.com/view/lunges-exercise-workout-leg-exercise-working-out-gif-17352592',
+          'https://media.tenor.com/fWiC9Ze5eUMAAAAC/lunges-exercise.gif',
         type: 'Equilibrio',
         muscleGroupId: 2,
         createdAt: new Date(),
@@ -215,7 +46,7 @@ async function dbConnection() {
         description:
           'Abre las piernas, mirando al frente y espalda siempre recta. Baja la pélvis hasta debajo de la altura de las rodillas y cierra los brazos juntando las manos delante de la cara.',
         picture:
-          'https://tenor.com/view/sumo-squat-exercise-workout-working-out-gif-17352727',
+          'https://media.tenor.com/jAjshaoXrewAAAAC/sumo-squat-exercise.gif',
         type: 'Flexibilidad',
         muscleGroupId: 2,
         createdAt: new Date(),
@@ -224,7 +55,7 @@ async function dbConnection() {
         name: 'Extensión rodilla-mancuernas',
         description:
           'Erguido, coge una mancuerna en cada mano y colócalas, con los brazos extendidos, delante de loscuádriceps. Agacha la espalda, siempre recta, doblando ligeramente las rodillas.',
-        picture: 'https://tenor.com/view/workouts-deadlift-gif-24035559',
+        picture: 'https://media.tenor.com/T-L7dxrLv7EAAAAC/workouts-deadlift.gif',
         type: 'Flexibilidad',
         muscleGroupId: 3,
         createdAt: new Date(),
@@ -233,7 +64,7 @@ async function dbConnection() {
         name: 'Remo con mancuernas',
         description:
           'Coge una mancuerna en cada mano. De pie, agacha la espalda, siempre recta, y extiende los brazos hacia abajo de modo que apunten al suelo. Trae las mancuernas hacia el pecho usando la espalda y doblando el codo.',
-        picture: 'https://tenor.com/view/dumbbell-row-standing-gif-10530352',
+        picture: 'https://media.tenor.com/XehF1R8EzM4AAAAC/dumbbell-row.gif',
         type: 'Fuerza',
         muscleGroupId: 3,
         createdAt: new Date(),
@@ -242,7 +73,7 @@ async function dbConnection() {
         name: 'Press banca inclinado mancuerna',
         description:
           'Coge una mancuerna en cada mano. Acuéstate en el banco con el respaldo reclinado. Estira ambos brazos hacia el techo y repite.',
-        picture: 'https://tenor.com/view/2inclne-dumbel-press-gif-26653346',
+        picture: 'https://media.tenor.com/9T1dx6LbbgwAAAAC/2inclne-dumbel-press.gif',
         type: 'Fuerza',
         muscleGroupId: 4,
         createdAt: new Date(),
@@ -251,7 +82,7 @@ async function dbConnection() {
         name: 'Press banca barra horizontal',
         description:
           'Acuéstate horizontalmente en un banco. Con las dos manos, coge la barra y elévala hacia el techo, extendiendo ambos brazo. Baja hasta tocar el pecho y repite.',
-        picture: 'https://tenor.com/view/bench-press-gif-26543726',
+        picture: 'https://media.tenor.com/0hoNLcggDG0AAAAC/bench-press.gif',
         type: 'Fuerza',
         muscleGroupId: 4,
         createdAt: new Date(),
@@ -260,7 +91,7 @@ async function dbConnection() {
         name: 'Press Arnold',
         description:
           'Sentado en un banco, coge una mancuerna en cada mano. Eléva los brazos por encima de la cabeza. Al bajarlos, trae las mancuernas hacia delante de la cara girando las muñecas de tal forma que las palmas estén mirando hacia adentro.',
-        picture: 'https://tenor.com/view/arnold-press-gif-25588656',
+        picture: 'https://media.tenor.com/ZR-_3Mxq0gYAAAAC/arnold-press.gif',
         type: 'Fuerza',
         muscleGroupId: 5,
         createdAt: new Date(),
@@ -270,7 +101,7 @@ async function dbConnection() {
         description:
           'Coge una mancuerna en cada mano. De pie, agacha la espalda hacia delante. Con los brazos estidado y las manos enfrentadas, eleva los brazos lateralmente, como las alas de un pájaro.',
         picture:
-          'https://tenor.com/view/rear-raise-rear-rear-raise-gym-gif-27091664',
+          'https://media.tenor.com/HTvjufujuJAAAAAC/rear-raise-rear.gif',
         type: 'Fuerza',
         muscleGroupId: 5,
         createdAt: new Date(),
@@ -379,7 +210,6 @@ async function dbConnection() {
 
     await connection.query(`USE ${MYSQL_DATABASE};`);
 
->>>>>>> fc66d9314466c6f29eba576a0629951af5a2a84a
     //Tabla de usuarios
     await connection.query(`
             CREATE TABLE IF NOT EXISTS users (
@@ -437,8 +267,6 @@ async function dbConnection() {
             );
         `);
 
-<<<<<<< HEAD
-=======
     //Tabla de ejercicios de entrenamiento
     await connection.query(`
     CREATE TABLE IF NOT EXISTS workouts_exercises (
@@ -450,7 +278,6 @@ async function dbConnection() {
         );
     `);
 
->>>>>>> fc66d9314466c6f29eba576a0629951af5a2a84a
     //Tabla de likes
     await connection.query(`
         CREATE TABLE IF NOT EXISTS likes (
@@ -467,42 +294,14 @@ async function dbConnection() {
 
     // Insertamos el usuario administrador
     await connection.query(
-<<<<<<< HEAD
-        `
-            INSERT INTO users (name, email, password, admin, createdAt) VALUES ("admin", "admin@admin.com", "${hashedPass}", true, ?)
-        `,
-        [new Date()]
-=======
       `
             INSERT INTO users (name, email, password, admin, createdAt) VALUES ("admin", "admin@admin.com", "${hashedPass}", true, ?)
         `,
       [new Date()]
->>>>>>> fc66d9314466c6f29eba576a0629951af5a2a84a
     );
 
     // Agregamos los grupos musculares.
     await connection.query(
-<<<<<<< HEAD
-        `INSERT INTO muscleGroup (name, createdAt) VALUES ("brazo", ?)`,
-        [new Date()]
-    );
-
-    await connection.query(
-        `INSERT INTO muscleGroup (name, createdAt) VALUES ("pierna", ?)`,
-        [new Date()]
-    );
-    await connection.query(
-        `INSERT INTO muscleGroup (name, createdAt) VALUES ("espalda", ?)`,
-        [new Date()]
-    );
-    await connection.query(
-        `INSERT INTO muscleGroup (name, createdAt) VALUES ("pecho", ?)`,
-        [new Date()]
-    );
-    await connection.query(
-        `INSERT INTO muscleGroup (name, createdAt) VALUES ("hombro", ?)`,
-        [new Date()]
-=======
       `INSERT INTO muscleGroup (name, createdAt) VALUES ("brazo", ?)`,
       [new Date()]
     );
@@ -522,7 +321,6 @@ async function dbConnection() {
     await connection.query(
       `INSERT INTO muscleGroup (name, createdAt) VALUES ("hombro", ?)`,
       [new Date()]
->>>>>>> fc66d9314466c6f29eba576a0629951af5a2a84a
     );
     await insertExercises(connection);
 
