b8f1ef34c98907873b1d61bd065309c205c21f58
Will SRamos <97808222+m3g47r0n@users.noreply.github.com>
2023-09-20 13:01:31 +0200

    Add files via upload
    Modificación del nombre de las rutas (user en lugar de users)

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors({origin:[
-    'https://warrior-gym.es',
-    'https://localhost:3000',
-    'https://localhost:5173',
-    'http://localhost:5173'
-]}));
+app.use(
+  cors({
+    origin: [
+      'https://warrior-gym.es',
+      'http://localhost:3000',
+      'http://localhost:5173',
+    ],
+  })
+);
 
 //Controllers
 const { newUser } = require('./src/controllers/users/newUser');
@@ -23,14 +26,27 @@ const { newGoal } = require('./src/controllers/goals/addGoals');
 const { getWorkoutGoals } = require('./src/controllers/goals/getGoalsWorkouts');
 const { getAllGoals } = require('./src/controllers/goals/getAllGoals');
 
-const { newMuscleGroup } = require('./src/controllers/muscleGroup/addMuscleGroup');
-const { getMuscleExercises } = require('./src/controllers/muscleGroup/getMuscleExercises');
-const { getAllMuscles } = require('./src/controllers/muscleGroup/getAllMuscleGroups');
+const {
+  newMuscleGroup,
+} = require('./src/controllers/muscleGroup/addMuscleGroup');
+const {
+  getMuscleExercises,
+} = require('./src/controllers/muscleGroup/getMuscleExercises');
+const {
+  getAllMuscles,
+} = require('./src/controllers/muscleGroup/getAllMuscleGroups');
 
 const { newExercise } = require('./src/controllers/exercises/newExercises');
 const { getExercise } = require('./src/controllers/exercises/getExercises');
-const { deleteExercise } = require('./src/controllers/exercises/deleteExercises');
-const { getAllExercises } = require('./src/controllers/exercises/getAllExercises');
+const {
+  deleteExercise,
+} = require('./src/controllers/exercises/deleteExercises');
+const {
+  getAllExercises,
+} = require('./src/controllers/exercises/getAllExercises');
+const {
+  addExercisesInWorkout,
+} = require('./src/controllers/exercises/addExercisesInWorkouts');
 
 const { newWorkout } = require('./src/controllers/workouts/newWorkout');
 const { getWorkout } = require('./src/controllers/workouts/getWorkout');
@@ -40,7 +56,9 @@ const { deleteWorkout } = require('./src/controllers/workouts/deleteWorkout');
 
 const { likeDislike } = require('./src/controllers/likes/likeDislike');
 const { getLikes } = require('./src/controllers/likes/getLike');
-const { getLikedExercises } = require('./src/controllers/likes/getLikedExercises');
+const {
+  getLikedExercises,
+} = require('./src/controllers/likes/getLikedExercises');
 
 //Valida la información introducida en el body
 const validateInfo = require('./src/middleware/validateInfo');
@@ -57,10 +75,10 @@ const checkToken = require('./src/middleware/checkToken');
 app.post('/users/register', validateInfo(registerSchema), newUser);
 
 // Devuelve usuario por su id
-app.get('/users/:id', getUser);
+app.get('/user', checkToken, getUser);
 
 // Ingreso de usuario creado
-app.post('/users/login', validateInfo(loginSchema), login);
+app.post('/user/login', validateInfo(loginSchema), login);
 
 // Crea un objetivo nuevo
 app.post('/goals', checkToken, validateInfo(goalsMuscleSchema), newGoal);
@@ -72,7 +90,12 @@ app.get('/goals/:id', checkToken, getWorkoutGoals);
 app.get('/goals', checkToken, getAllGoals);
 
 //Crea grupo muscular
-app.post('/muscleGroups', checkToken, validateInfo(goalsMuscleSchema), newMuscleGroup);
+app.post(
+  '/muscleGroup',
+  checkToken,
+  validateInfo(goalsMuscleSchema),
+  newMuscleGroup
+);
 
 //Devuelve ejercicios con el mismo grupo muscular
 app.get('/muscleGroups/:id', checkToken, getMuscleExercises);
@@ -87,7 +110,7 @@ app.post('/exercises', checkToken, validateInfo(exerciseSchema), newExercise);
 app.get('/exercises/:id', checkToken, getExercise);
 
 //Devuelve todos los ejercicios
-app.get('/exercises', checkToken, getAllExercises)
+app.get('/exercises', checkToken, getAllExercises);
 
 // Elimina ejercicio
 app.delete('/exercises/:id', checkToken, deleteExercise);
@@ -102,10 +125,12 @@ app.get('/workouts/:id', checkToken, getWorkout);
 app.get('/workouts', checkToken, getAllWorkouts);
 
 // Modifica entrenamiento
-app.put('/workouts/:id' , checkToken, modifyWorkout);
+app.put('/workouts/:id', checkToken, modifyWorkout);
 
 // Elimina entrenamiento creado
 app.delete('/workouts/:id', checkToken, deleteWorkout);
+
+app.post('/exercisesworkouts', checkToken, addExercisesInWorkout);
 
 // Introduce like o borra el mismo si ya existe
 app.post('/likes/:id', checkToken, likeDislike);
@@ -114,31 +139,28 @@ app.post('/likes/:id', checkToken, likeDislike);
 // cuando hagamos un GET a "/exercises/:id"
 app.get('/likes/:id', checkToken, getLikes);
 
-//Devuelve cantidad de likes por ejercicio
-app.get('/likes/:id', checkToken, getLikes);
-
 //Obtiene los ejercicios con "me gusta" del usuario actual
 app.get('/likes', checkToken, getLikedExercises);
 
 // Middleware de 404
 app.use((req, res) => {
-    res.status(404).send({
-        status: 'error',
-        message: 'Not found',
-    });
+  res.status(404).send({
+    status: 'error',
+    message: 'Not found',
+  });
 });
 
 // Middleware de gestión de errores
 app.use((error, req, res, next) => {
-    console.error(error);
+  console.error(error);
 
-    res.status(error.httpStatus || 500).send({
-        status: 'error',
-        message: error.message,
-    });
+  res.status(error.httpStatus || 500).send({
+    status: 'error',
+    message: error.message,
+  });
 });
 
 // Lanzamos el servidor
 app.listen(3000, () => {
-    console.log('Servidor funcionando');
+  console.log('Servidor funcionando');
 });
