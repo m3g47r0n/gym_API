50476186bc02e4567489234d308cd5f31bc3f7bf
Will SRamos <97808222+m3g47r0n@users.noreply.github.com>
2023-09-20 13:04:32 +0200

    Add files via upload
    Cambios en archivos para identificar si un usuario es admin o no y que este valor pueda ser obtenido desde el front para renderizar el botón de añadir ejercicios si el usuario logeado es admin

--- src/controllers/users/getUser.js
+++ src/controllers/users/getUser.js
@@ -1,43 +1,47 @@
 const { generateError } = require('../../middleware/helpers');
 const { getConnection } = require('../../database/db');
+const jwt = require('jsonwebtoken');
 
 // Devuelve info de user por su id
 const getUserById = async (id) => {
-    let connection;
-    try {
-        //Conexión con la base de datos
-        connection = await getConnection();
-        const [result] = await connection.query(`
-        SELECT name, email, createdAt FROM users WHERE id = ?
-        `, [id], 
-        );
+  let connection;
+  try {
+    //Conexión con la base de datos
+    connection = await getConnection();
+    const [result] = await connection.query(
+      `
+        SELECT id, name, email, createdAt, admin FROM users WHERE id = ?
+        `,
+      [id]
+    );
 
-        if (result.length === 0) {
-            throw generateError('No existe usuario con ese id.', 404)
-        }
-
-        return result[0];
-
-    } finally {
-        // Si existe conexión, se libera
-        if (connection) connection.release();
+    if (result.length === 0) {
+      throw generateError('No existe usuario con ese id.', 404);
     }
+
+    return result[0];
+  } finally {
+    // Si existe conexión, se libera
+    if (connection) connection.release();
+  }
 };
 
 const getUser = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const user = await getUserById(id);
-        res.send({
-            status: 'ok',
-            data: user
-        });
+  try {
+    const token = req.headers.authorization;
+    const payload = jwt.verify(token, process.env.SECRET);
+    const userId = payload.id;
 
-    } catch(error) {
-        next(error);
-    }
+    const user = await getUserById(userId);
+    res.send({
+      status: 'ok',
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-    getUser
-}
+  getUser,
+};
